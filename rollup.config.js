16f95726aa48b647353a06eb04666d04fab10303
Evgeniy Fedoryachenko <mdwit0r@gmail.com>
2023-06-20 22:24:27 +0200

    build: add rollup configuration for building the library build: add typescript configuration for building the library build: add output formats for the library (cjs, esm, iife) feat: add default export class KinopoiskDev to index.ts chore: add dist folder to .gitignore

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'dist/bundle.cjs.js',
+            format: 'cjs',
+        },
+        {
+            file: 'dist/bundle.esm.js',
+            format: 'esm',
+        },
+        {
+            file: 'dist/bundle.iife.js',
+            format: 'iife',
+            name: 'kinopoiskdev',
+        }
+    ],
+    plugins: [
+        typescript(),
+        resolve(),
+        commonjs(),
+    ],
+};
