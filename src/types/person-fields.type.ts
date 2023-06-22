534d88ac88d9c176f082907e205cecb90618d0a5
Evgeniy Fedoryachenko <mdwit0r@gmail.com>
2023-06-23 00:37:32 +0200

    feat(person-fields.type.ts): add more fields to PersonFields type definition

--- src/types/person-fields.type.ts
+++ src/types/person-fields.type.ts
@@ -1,8 +1,34 @@
 import { IQueryFields } from '../interfaces/query-fields.interface';
 
 export interface PersonFields extends IQueryFields {
-  StringFields: 'name' | 'enName';
-  DateFields: 'deathPlace.value';
-  NumberFields: 'id';
-  BooleanFields: string;
+  StringFields:
+    | 'name'
+    | 'enName'
+    | 'photo'
+    | 'sex'
+    | 'birthPlace.value'
+    | 'deathPlace.value'
+    | 'spouses.name'
+    | 'spouses.divorced'
+    | 'spouses.divorcedReason'
+    | 'spouses.sex'
+    | 'spouses.children'
+    | 'spouses.relation'
+    | 'profession.value'
+    | 'facts.value'
+    | 'movies.name'
+    | 'movies.alternativeName'
+    | 'movies.description'
+    | 'movies.enProfession';
+  DateFields: 'birthday' | 'death';
+  NumberFields:
+    | 'id'
+    | 'movies.id'
+    | 'movies.rating'
+    | 'age'
+    | 'countAwards'
+    | 'growth'
+    | 'spouses.id'
+    | 'spouses.children'
+    | 'spouses.name';
 }
