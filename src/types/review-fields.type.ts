2a6f222a9d56c11e831efd381b54a8a816908b7d
Evgeniy Fedoryachenko <mdwit0r@gmail.com>
2023-06-23 00:38:50 +0200

    feat(review-fields.type.ts): add ReviewFields type definition for query fields in reviews endpoint

---
+++ src/types/review-fields.type.ts
@@ -0,0 +1,7 @@
+import { IQueryFields } from '../interfaces/query-fields.interface';
+
+export interface ReviewFields extends IQueryFields {
+  StringFields: 'title' | 'type' | 'review' | 'author';
+  DateFields: 'date';
+  NumberFields: 'movieId' | 'id' | 'authorId';
+}
