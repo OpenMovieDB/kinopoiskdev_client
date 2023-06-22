c8f092a7ee5c820cf7b066e50ba747db5ebd54bf
Evgeniy Fedoryachenko <mdwit0r@gmail.com>
2023-06-23 00:39:19 +0200

    feat(season-fields.type.ts): add SeasonFields type to query episodes by fields

---
+++ src/types/season-fields.type.ts
@@ -0,0 +1,7 @@
+import { IQueryFields } from '../interfaces/query-fields.interface';
+
+export interface SeasonFields extends IQueryFields {
+  StringFields: 'episodes.name' | 'episodes.enName' | 'episodes.description';
+  DateFields: 'episodes.date';
+  NumberFields: 'movieId' | 'number' | 'episodesCount' | 'episodes.number';
+}
