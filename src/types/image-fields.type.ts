dcc54c93720945507b1a501e58afe1d95460f084
Evgeniy Fedoryachenko <mdwit0r@gmail.com>
2023-06-23 00:24:32 +0200

    feat(image-fields.type.ts): add ImageFields type to represent image query fields

---
+++ src/types/image-fields.type.ts
@@ -0,0 +1,6 @@
+import { IQueryFields } from '../interfaces/query-fields.interface';
+
+export interface ImageFields extends IQueryFields {
+  NumberFields: 'movieId' | 'height' | 'width';
+  StringFields: 'type' | 'url' | 'previewUrl' | 'language';
+}
