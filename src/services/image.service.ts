d45c97efc83bc5c2636a92aa8db2ea388481605b
Evgeniy Fedoryachenko <mdwit0r@gmail.com>
2023-06-24 01:46:15 +0200

    feat(image.service.ts): add getByFilters method to ImageService class

--- src/services/image.service.ts
+++ src/services/image.service.ts
@@ -1,6 +1,15 @@
 import { KinopoiskDev } from '../index';
 import { ClientRequest } from '../client-request';
+import { IResponse } from '../interfaces/response.interface';
+import { ImageDocsResponseDto } from '../interfaces/api.interface';
+import { VERSIONS } from '../enums/version.enum';
 
 export class ImageService {
   constructor(private readonly request: ClientRequest) {}
+
+  async getByFilters(
+    filters: Record<string, string>,
+  ): Promise<IResponse<ImageDocsResponseDto>> {
+    return await this.request.get(VERSIONS.V1, `/image`, filters);
+  }
 }
