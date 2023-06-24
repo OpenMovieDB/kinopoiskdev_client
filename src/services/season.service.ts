e62528acd9b3a54c933232d6e763fca3455e2cfe
Evgeniy Fedoryachenko <mdwit0r@gmail.com>
2023-06-24 02:18:43 +0200

    feat(season.service.ts): add getByFilters method to SeasonService class

--- src/services/season.service.ts
+++ src/services/season.service.ts
@@ -1,5 +1,16 @@
 import { ClientRequest } from '../client-request';
+import { IResponse } from '../interfaces/response.interface';
+import { SeasonDocsResponseDto } from '../interfaces/api.interface';
+import { VERSIONS } from '../enums/version.enum';
+import { Filter } from '../interfaces/query-builder.interface';
+import { SeasonFields } from '../types/season-fields.type';
 
 export class SeasonService {
   constructor(private readonly request: ClientRequest) {}
+
+  async getByFilters(
+    filters: Filter<SeasonFields>,
+  ): Promise<IResponse<SeasonDocsResponseDto>> {
+    return await this.request.get(VERSIONS.V1, `/season`, filters);
+  }
 }
