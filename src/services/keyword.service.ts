7efb0b749fb2cd0081adf6534518f29550d9a2c7
Evgeniy Fedoryachenko <mdwit0r@gmail.com>
2023-06-24 02:16:26 +0200

    feat(keyword.service.ts): add getByFilters method to KeywordService class

--- src/services/keyword.service.ts
+++ src/services/keyword.service.ts
@@ -1,6 +1,16 @@
-import { KinopoiskDev } from '../index';
 import { ClientRequest } from '../client-request';
+import { IResponse } from '../interfaces/response.interface';
+import { KeywordDocsResponseDto } from '../interfaces/api.interface';
+import { VERSIONS } from '../enums/version.enum';
+import { Filter } from '../interfaces/query-builder.interface';
+import { KeywordFields } from '../types/keyword-fields.type';
 
 export class KeywordService {
   constructor(private readonly request: ClientRequest) {}
+
+  async getByFilters(
+    filters: Filter<KeywordFields>,
+  ): Promise<IResponse<KeywordDocsResponseDto>> {
+    return await this.request.get(VERSIONS.V1, `/keyword`, filters);
+  }
 }
