b8e9d123b7ff1bb5170f66b32e94f5dfc637a24e
Evgeniy Fedoryachenko <mdwit0r@gmail.com>
2023-06-24 02:18:33 +0200

    feat(studio.service.ts): add methods to get studio by id and by filters

--- src/services/studio.service.ts
+++ src/services/studio.service.ts
@@ -1,5 +1,19 @@
 import { ClientRequest } from '../client-request';
+import { IResponse } from '../interfaces/response.interface';
+import { Studio, StudioDocsResponseDto } from '../interfaces/api.interface';
+import { VERSIONS } from '../enums/version.enum';
+import { Filter } from '../interfaces/query-builder.interface';
+import { StudioFields } from '../types/studio-fields.type';
 
 export class StudioService {
   constructor(private readonly request: ClientRequest) {}
+
+  async getById(id: number): Promise<IResponse<Studio>> {
+    return await this.request.get(VERSIONS.V1, `/studio/${id}`);
+  }
+  async getByFilters(
+    filters: Filter<StudioFields>,
+  ): Promise<IResponse<StudioDocsResponseDto>> {
+    return await this.request.get(VERSIONS.V1, `/studio`, filters);
+  }
 }
