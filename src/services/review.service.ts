28c6ab01d371d20ddc4e68a7fed1aa313a67661e
Evgeniy Fedoryachenko <mdwit0r@gmail.com>
2023-06-24 02:18:51 +0200

    feat(review.service.ts): add getByFilters method to ReviewService class

--- src/services/review.service.ts
+++ src/services/review.service.ts
@@ -1,5 +1,16 @@
 import { ClientRequest } from '../client-request';
+import { IResponse } from '../interfaces/response.interface';
+import { ReviewDocsResponseDto } from '../interfaces/api.interface';
+import { VERSIONS } from '../enums/version.enum';
+import { Filter } from '../interfaces/query-builder.interface';
+import { ReviewFields } from '../types/review-fields.type';
 
 export class ReviewService {
   constructor(private readonly request: ClientRequest) {}
+
+  async getByFilters(
+    filters: Filter<ReviewFields>,
+  ): Promise<IResponse<ReviewDocsResponseDto>> {
+    return await this.request.get(VERSIONS.V1, `/review`, filters);
+  }
 }
