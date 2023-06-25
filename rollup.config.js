18f9f90bc49fb6dbde9c480b5c9d2345b583088a
Evgeniy Fedoryachenko <mdwit0r@gmail.com>
2023-06-25 19:59:35 +0200

    fix: commonjs causing conflicts

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/bundle.esm.js',
-      format: 'esm',
-    },
-    {
-      file: 'dist/bundle.iife.js',
-      format: 'iife',
-      name: 'kinopoiskdev',
-    },
-  ],
-  plugins: [typescript(), resolve()],
+  output: {
+    file: 'dist/bundle.umd.js',
+    format: 'umd',
+    name: 'kinopoiskdev_client',
+    globals: {}
+  },
+  external: [],
+  plugins: [
+    resolve(),
+    typescript({
+      tsconfig: 'tsconfig.json'
+    }),
+  ]
 };
